eebb04cb8f3d47d7b9638d744b07bbdf3bd3db09
nuyeo <gooywoo@gachon.ac.kr>
2024-04-22 13:36:25 +0900

    feat: server.js 파일에 서버 라우팅 추가

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 const express = require('express');
+
+const app = express();
+const port = 60023;
+
+const server = app.listen(port, () => {
+    const { address, port } = server.address();
+    console.log(`Server is running on http://${address}:${port}`);
+})
