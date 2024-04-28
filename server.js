3e4d565b03e26acb5e3ba98938ec36dc6a0e8bcd
soojxng <modify0012@gmail.com>
2024-04-28 17:28:50 +0900

    Add: server.js 파일에 ejs 사용 설정 추가

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 60023;
 
+const ejs = require("ejs");
+app.set("view engine", "ejs")
+// app.set("views", /* ejs 파일 경로 "./views" */)
+
 const server = app.listen(port, () => {
     const { address, port } = server.address();
     console.log(`Server is running on http://${address}:${port}`);
