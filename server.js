bb5dfb453daf43620d10436bb2a937775e8bfff6
soojxng <modify0012@gmail.com>
2024-04-28 22:29:56 +0900

    Add: swagger 세팅

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 const app = express();
 const port = 60023;
 
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: '주독야독 API',
+            version: '1.0.0',
+            description: '주독야독 API 문서',
+        },
+    },
+    apis: ['./routes/admin/*.js'],  // 필요 시 배열 형식으로 파일 경로 추가
+}
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+// Swagger UI 설정
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 const server = app.listen(port, () => {
     const { address, port } = server.address();
     console.log(`Server is running on http://${address}:${port}`);
+    console.log(`Swagger --> http://${address}:${port}/api-docs`);
 })
