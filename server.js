7a624261e3dbd6743fbf29f124584dc388b13a12
soojxng <modify0012@gmail.com>
2024-04-28 17:33:03 +0900

    Add: 관리자 기능 라우터 활성화 추가

--- server.js
+++ server.js
@@ -7,6 +7,28 @@ const ejs = require("ejs");
 app.set("view engine", "ejs")
 // app.set("views", /* ejs 파일 경로 "./views" */)
 
+/* 관리자 기능 라우터 */
+// 관리자 메인 화면 로드
+const adminMainRouter = require('./routes/admin/main');
+// 관리자 인증
+const adminAuthRouter = require('./routes/admin/auth');
+// 회원 관리
+const manageUserRouter = require('./routes/admin/users');
+// 지문 관리
+const manageTextRouter = require('./routes/admin/texts');
+// 피드백 관리
+const manageFeedbackRouter = require('./routes/admin/feedback');
+// AI 관리
+// const manageAiRouter = require('./routes/admin/ai');
+
+// 관리자 기능 router 활성화
+app.use('/admin', adminMainRouter);
+app.use('/admin', adminAuthRouter);
+app.use('/admin/users', manageUserRouter);
+app.use('/admin/text', manageTextRouter);
+app.use('/admin/feedback', manageFeedbackRouter);
+// app.use('/admin/ai', manageAiRouter);
+
 const server = app.listen(port, () => {
     const { address, port } = server.address();
     console.log(`Server is running on http://${address}:${port}`);
