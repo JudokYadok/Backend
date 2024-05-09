515b9e6ad62181f90a57388ed24ff35d1a43fb2a
nuyeo <gooywoo@gachon.ac.kr>
2024-05-09 15:11:25 +0900

    Feat: user/기출 지문 조회 api

--- routes/user/study/prevtext.js
+++ routes/user/study/prevtext.js
@@ -41,7 +41,32 @@ const selectPrevText = (req, res) => {
     });
 };
 
+// 기출 지문 조회
+const viewPrevText = (req, res) => {
+    const { category, text_id } = req.params.category; // URL 파라미터에서 category, text_id 추출
+  
+    text.findOne({
+        attributes: ['title', 'year', 'contents'],
+        where: {
+            user_id: 1, // 관리자 지문만 검색
+            category: category, // 주어진 category에 해당하는 행만 검색
+            text_id: text_id
+      }
+    })
+    .then(prevText => {
+        if (prevText) {
+            res.json(prevText); // 조회된 지문을 JSON 형태로 응답
+        } else {
+            res.status(404).json({ error: 'Prevtext not found' }); // 해당 text_id에 해당하는 Prevtext가 없는 경우
+        }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Failed to fetch previous texts' }); // 오류 발생 시 500 에러 응답
+    });
+};
+
 router.get("/user/study/prevtext", selectPrevtextCategory);
 router.get("/user/study/prevtext/:category", selectPrevText);
+router.get("/user/study/prevtext/:category/:text_id", viewPrevText);
 
 module.exports = router;
