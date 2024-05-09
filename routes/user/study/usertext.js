f6c98c96498363de314c09d7e715074d81f03e8e
nuyeo <gooywoo@gachon.ac.kr>
2024-05-09 15:04:35 +0900

    Feat: user/사용자 지문 선택 화면 조회 api

--- routes/user/study/usertext.js
+++ routes/user/study/usertext.js
@@ -23,5 +23,25 @@ const selectMytextCategory = (req, res) => {
     });
 };
 
+// 사용자 지문 선택 화면 조회
+const selectMyText = (req, res) => {
+    const category = req.params.category; // URL 파라미터에서 category 추출
+  
+    text.findAll({
+        attributes: ['title', 'year'],
+        where: {
+            // user_id: user_id, // 해당 사용자 지문만 검색
+            category: category // 주어진 category에 해당하는 행만 검색
+      }
+    })
+    .then(MyTexts => {
+      res.json(MyTexts); // 조회된 지문 리스트를 JSON 형태로 응답
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Failed to fetch user texts' }); // 오류 발생 시 500 에러 응답
+    });
+};
+
 router.get("/user/study/Mytext", selectMytextCategory);
+router.get("/user/study/Mytext/:category", selectMyText);
 module.exports = router;
