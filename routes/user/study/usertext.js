a77f65511ff85a6cf736bc858ce8231e1687babd
nuyeo <gooywoo@gachon.ac.kr>
2024-05-09 15:05:52 +0900

    Feat: user/사용자 지문 조회 api

--- routes/user/study/usertext.js
+++ routes/user/study/usertext.js
@@ -42,6 +42,31 @@ const selectMyText = (req, res) => {
     });
 };
 
+// 사용자 지문 조회
+const viewMyText = (req, res) => {
+    const { category, text_id } = req.params; // URL 파라미터에서 category, text_id 추출
+  
+    text.findOne({
+        attributes: ['title', 'year', 'contents'],
+        where: {
+            // user_id: user_id, // 해당 사용자 지문만 검색
+            category: category, // 주어진 category에 해당하는 행만 검색
+            text_id: text_id
+      }
+    })
+    .then(MyText => {
+        if (MyText) {
+            res.json(MyText); // 조회된 지문을 JSON 형태로 응답
+        } else {
+            res.status(404).json({ error: 'Usertext not found' }); // 해당 text_id에 해당하는 Mytext가 없는 경우
+        }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Failed to fetch user texts' }); // 오류 발생 시 500 에러 응답
+    });
+};
+
 router.get("/user/study/Mytext", selectMytextCategory);
 router.get("/user/study/Mytext/:category", selectMyText);
+router.get("/user/study/Mytext/:category/:text_id", viewMyText);
 module.exports = router;
