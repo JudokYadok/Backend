b2b92a5b50f30073d86288bca086c62812852ff1
nuyeo <gooywoo@gachon.ac.kr>
2024-05-12 19:06:55 +0900

    Rename: middleware.js 위치 이동

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,8 +1,19 @@
+const jwt = require('./jwt');
+
 function adminRequire(req, res, next){
     if(req.session.user_id) {
         next();
     }else{
         res.redirect('/admin/login');
+    }
+}
+
+function tokenRequire(token, next) {
+    const result = jwt.verify(token);
+    if(result.ok){
+        next();
+    } else {
+        
     }
 }
 
