fd355339e29cc7d37770b89206c6f18a93af6edc
nuyeo <gooywoo@gachon.ac.kr>
2024-05-12 18:55:29 +0900

    Refactor: 비동기 작업 Promise 처리

--- utils/jwt.js
+++ utils/jwt.js
@@ -43,17 +43,27 @@ module.exports = {
         try {
             const query = `SELECT refresh FROM token WHERE user_id = ?`;
 
-            req.conn.query(query, user_id, (err, results) => {
-                if (err) {
-                    console.error(err);
-                } else {
-                    if(results[0].refresh === token){
-                        jwt.verify(token, secret);
-                        return true;
+            return new Promise((resolve, reject) => {
+                req.conn.query(query, user_id, (err, results) => {
+                    if (err) {
+                        console.error(err);
+                        reject(err); // 오류 발생 시 reject 호출
                     } else {
-                        return false;
+                        if(results[0].refresh === token){
+                            // 토큰이 일치할 경우 resolve 호출하여 검증 결과 반환
+                            jwt.verify(token, secret, (err, decoded) => {
+                                if (err) {
+                                    console.error(err);
+                                    reject(err); // 검증 실패 시 reject 호출
+                                } else {
+                                    resolve(decoded); // 검증 성공 시 resolve 호출하여 결과 반환
+                                }
+                            });
+                        } else {
+                            resolve(false); // 토큰이 일치하지 않을 경우 false 반환
+                        }
                     }
-                }
+                });
             });
         } catch (err) {
             console.error(err)
